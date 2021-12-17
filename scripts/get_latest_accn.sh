1cd7dce6dac23c336231e6c1e534b2f6eb781c05
Daniel Ruzeu <druzeu@gmail.com>
2021-12-17 17:01:41 +1100

    quotes changed and added -c to wget

--- scripts/get_latest_accn.sh
+++ scripts/get_latest_accn.sh
@@ -3,9 +3,9 @@ wget https://www.dl.dropboxusercontent.com/s/6fx39oixavoqcgt/services.json
 Record=1
 serviceDate=`jq ".services[${Record}]" services.json | jq '.["serviceDate"]' | sed 's/st//g;s/nd//g;s/rd//g;s/th//g;s/\"//g'`
 Date=`date "+%Y-%m-%d" -d "$serviceDate"`
-morningRecording=`jq ".services[${Record}]" services.json | jq '.["morningRecording"]'`
+morningRecording=`jq ".services[${Record}]" services.json | jq '.["morningRecording"]' | sed 's/\"//g'`
 morningMinisters=`jq ".services[${Record}]" services.json | jq '.["morningMinisters"]' | sed 's/\((.*)\)//g;s/,/_/g;s/\"//g;s/\ //g'`
-afternoonRecording=`jq ".services[${Record}]" services.json | jq '.["afternoonRecording"]'`
+afternoonRecording=`jq ".services[${Record}]" services.json | jq '.["afternoonRecording"]' | sed 's/\"//g'`
 afternoonMinisters=`jq ".services[${Record}]" services.json | jq '.["afternoonMinisters"]' | sed 's/\((.*)\)//g' | sed 's/,/_/g' | sed 's/\ //g' | sed 's/\"//g'`
 eveningRecording=`jq ".services[${Record}]" services.json | jq '.["eveningRecording"]'`
 FilenameAM="ACCN-$morningMinisters-$Date AM"
@@ -25,7 +25,8 @@ FilenameSinging="ACCN-Singing-$Date"
 #echo id3tool -t "$FilenameAM" -r "$morningMinisters" -y `date "+%Y"` "$FilenameAM.mp3"
 #echo wget $afternoonRecording -O $FilenamePM.mp3
 #echo id3tool -t "$FilenamePM" -r "$afternoonMinisters" -y `date "+%Y"` "$FilenamePM.mp3"
-wget $morningRecording -O $FilenameAM.mp3
-id3tool -t "$FilenameAM" -r "$morningMinisters" -y `date "+%Y"` "$FilenameAM.mp3"
-wget $afternoonRecording -O $FilenamePM.mp3
-id3tool -t "$FilenamePM" -r "$afternoonMinisters" -y `date "+%Y"` "$FilenamePM.mp3"
+wget -c -O "$FilenameAM.mp3" $morningRecording
+id3tool -t \"$FilenameAM\" -r "$morningMinisters" -y `date "+%Y"` \"$FilenameAM.mp3\"
+wget -c -O "$FilenamePM.mp3" $afternoonRecording
+id3tool -t \"$FilenamePM\" -r "$afternoonMinisters" -y `date "+%Y"` \"$FilenamePM.mp3\"
+rm services.json
