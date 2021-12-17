4a421f63d2cc05249a29966101035b26ab331cba
Daniel Ruzeu <druzeu@gmail.com>
2021-12-17 17:45:43 +1100

    Change PWD reserved variable.

--- scripts/get_latest_accn.sh
+++ scripts/get_latest_accn.sh
@@ -7,15 +7,15 @@ else
   Record=$1
 fi
 #jq ".services[${Record}]" services.json
-PWD=`pwd`
+Current=`pwd`
 cd /var/lib/minidlna/movies/Other/Church/Services/2021tillNow
-serviceDate=`jq ".services[${Record}]" $PWD\services.json | jq '.["serviceDate"]' | sed 's/st//g;s/nd//g;s/rd//g;s/th//g;s/\"//g'`
+serviceDate=`jq ".services[${Record}]" $Current\services.json | jq '.["serviceDate"]' | sed 's/st//g;s/nd//g;s/rd//g;s/th//g;s/\"//g'`
 Date=`date "+%Y-%m-%d" -d "$serviceDate"`
-morningRecording=`jq ".services[${Record}]" $PWD\services.json | jq '.["morningRecording"]' | sed 's/\"//g'`
-morningMinisters=`jq ".services[${Record}]" $PWD\services.json | jq '.["morningMinisters"]' | sed 's/\((.*)\)//g;s/,/_/g;s/\"//g;s/\ //g'`
-afternoonRecording=`jq ".services[${Record}]" $PWD\services.json | jq '.["afternoonRecording"]' | sed 's/\"//g'`
-afternoonMinisters=`jq ".services[${Record}]" $PWD\services.json | jq '.["afternoonMinisters"]' | sed 's/\((.*)\)//g' | sed 's/,/_/g' | sed 's/\ //g' | sed 's/\"//g'`
-eveningRecording=`jq ".services[${Record}]" $PWD\services.json | jq '.["eveningRecording"]' | sed 's/\"//g'`
+morningRecording=`jq ".services[${Record}]" $Current\services.json | jq '.["morningRecording"]' | sed 's/\"//g'`
+morningMinisters=`jq ".services[${Record}]" $Current\services.json | jq '.["morningMinisters"]' | sed 's/\((.*)\)//g;s/,/_/g;s/\"//g;s/\ //g'`
+afternoonRecording=`jq ".services[${Record}]" $Current\services.json | jq '.["afternoonRecording"]' | sed 's/\"//g'`
+afternoonMinisters=`jq ".services[${Record}]" $Current\services.json | jq '.["afternoonMinisters"]' | sed 's/\((.*)\)//g' | sed 's/,/_/g' | sed 's/\ //g' | sed 's/\"//g'`
+eveningRecording=`jq ".services[${Record}]" $Current\services.json | jq '.["eveningRecording"]' | sed 's/\"//g'`
 FilenameAM="ACCN-$morningMinisters-$Date AM"
 FilenamePM="ACCN-$afternoonMinisters-$Date PM"
 FilenameSinging="ACCN-Singing-$Date"
