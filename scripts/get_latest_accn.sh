7767d33b27423293e15826f76d4a3554c3fecb7e
Daniel Ruzeu <druzeu@gmail.com>
2021-12-17 16:47:25 +1100

    Updated to use any record defined by Record=, the default is 0

--- scripts/get_latest_accn.sh
+++ scripts/get_latest_accn.sh
@@ -1,39 +1,31 @@
 #!/bin/bash
-DaySuffix() {
-#  case `date +%d` in
-  x="$*"
-  case $x in
-    1|21|31) echo "st";;
-    2|22)    echo "nd";;
-    3|23)    echo "rd";;
-    *)       echo "th";;
-  esac
-}
-#for (( i = 1; i <= 31; i++))
-#do
-#	echo $i`DaySuffix "$i"`
-#done
-#date "+%d`DaySuffix date +%d` %B %Y" -d "last Sun"
-Date=`date "+%Y-%m-%d" -d "last Sun"`
-Record=0
+wget https://www.dl.dropboxusercontent.com/s/6fx39oixavoqcgt/services.json
+Record=1
+serviceDate=`jq ".services[${Record}]" services.json | jq '.["serviceDate"]' | sed 's/st//g;s/nd//g;s/rd//g;s/th//g;s/\"//g'`
+Date=`date "+%Y-%m-%d" -d "$serviceDate"`
 morningRecording=`jq ".services[${Record}]" services.json | jq '.["morningRecording"]'`
-morningMinisters=`jq ".services[${Record}]" services.json | jq '.["morningMinisters"]' | sed 's/\((.*)\)//g' | sed 's/,/_/g' | sed 's/\ //g' | sed 's/\"//g'`
+morningMinisters=`jq ".services[${Record}]" services.json | jq '.["morningMinisters"]' | sed 's/\((.*)\)//g;s/,/_/g;s/\"//g;s/\ //g'`
 afternoonRecording=`jq ".services[${Record}]" services.json | jq '.["afternoonRecording"]'`
 afternoonMinisters=`jq ".services[${Record}]" services.json | jq '.["afternoonMinisters"]' | sed 's/\((.*)\)//g' | sed 's/,/_/g' | sed 's/\ //g' | sed 's/\"//g'`
 eveningRecording=`jq ".services[${Record}]" services.json | jq '.["eveningRecording"]'`
 FilenameAM="ACCN-$morningMinisters-$Date AM"
 FilenamePM="ACCN-$afternoonMinisters-$Date PM"
 FilenameSinging="ACCN-Singing-$Date"
-echo $Date
-echo $morningRecording
-echo $morningMinisters
-echo $afternoonRecording
-echo $afternoonMinisters
-echo $eveningRecording
-echo $FilenameAM
-echo $FilenamePM
-echo $FilenameSinging
-echo wget $morningRecording -O $FilenameAM.mp3
-echo id3tool -t "$FilenameAM" -r "$morningMinisters" -y `date "+%Y"` "$FilenameAM.mp3"
-echo wget $afternoonRecording -O $FilenamePM.mp3
-echo id3tool -t "$FilenamePM" -r "$afternoonMinisters" -y `date "+%Y"` "$FilenamePM.mp3"
+#echo $Date
+#echo $serviceDate
+#echo $morningRecording
+#echo $morningMinisters
+#echo $afternoonRecording
+#echo $afternoonMinisters
+#echo $eveningRecording
+#echo $FilenameAM
+#echo $FilenamePM
+#echo $FilenameSinging
+#echo wget $morningRecording -O $FilenameAM.mp3
+#echo id3tool -t "$FilenameAM" -r "$morningMinisters" -y `date "+%Y"` "$FilenameAM.mp3"
+#echo wget $afternoonRecording -O $FilenamePM.mp3
+#echo id3tool -t "$FilenamePM" -r "$afternoonMinisters" -y `date "+%Y"` "$FilenamePM.mp3"
+wget $morningRecording -O $FilenameAM.mp3
+id3tool -t "$FilenameAM" -r "$morningMinisters" -y `date "+%Y"` "$FilenameAM.mp3"
+wget $afternoonRecording -O $FilenamePM.mp3
+id3tool -t "$FilenamePM" -r "$afternoonMinisters" -y `date "+%Y"` "$FilenamePM.mp3"
